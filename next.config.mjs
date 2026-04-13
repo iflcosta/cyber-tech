8ab64541f0dc50b78609e8b0cf02ecc3c43e8e44
iflcosta <iflcosta@gmail.com>
2026-04-13 14:54:58 -0300

    fix: force image display with unoptimized true

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -8,6 +9,7 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
+    domains: ['isrqhxhoabxkpunmbxti.supabase.co'],
   },
 };
 
